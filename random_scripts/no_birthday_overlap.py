2d7aff062c9eb5985a179e4595ce6c105eb3c191
Jarod DeWeese <jaroddeweese@gmail.com>
2020-09-23 21:13:24 -0500

    Perfect the math of birthday overlap

--- random_scripts/no_birthday_overlap.py
+++ random_scripts/no_birthday_overlap.py
@@ -2,7 +2,9 @@ from functools import reduce
 import operator
 num_ppl = 30
 days_in_year = 365
-iterable = (((days_in_year - i)/days_in_year) for i in range(num_ppl,0,-1))
+iterable = (((days_in_year - i)/days_in_year) for i in range(num_ppl-1,-1,-1))
 
 prob_no_overlap = reduce(operator.mul,iterable )
 print(prob_no_overlap)
+print('prob of overlap')
+print(1-prob_no_overlap)
