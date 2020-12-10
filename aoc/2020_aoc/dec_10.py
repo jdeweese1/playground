b9f1b0caae7ca07376192af14eb6669f37695a5a
Jarod DeWeese <jaroddeweese@gmail.com>
2020-12-09 22:01:10 -0600

    add day 10 and 11 solution stubs

---
+++ aoc/2020_aoc/dec_10.py
@@ -0,0 +1,18 @@
+import itertools as itrls
+
+import more_itertools as mi
+
+from aoc import aoc_utils as au
+
+inp = au.obtain_input_for_day('10')
+debug_path = au.get_debug_file_path('10')
+
+#%%
+test_cases = [
+    ('foobar', 1),
+]
+for in_data, correct in test_cases:
+    out_data = RuntimeError(in_data)
+    assert out_data == correct, f'{out_data} != {correct}'
+
+#%%
